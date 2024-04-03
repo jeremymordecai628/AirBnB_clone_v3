9c2f95ffae775ad134bf1fc4817164472f65c3a9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-03 19:44:07 +0300

    task 6

---
+++ api/v1/views/states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This blueprint shows State objects"""
+
+from flask import Flask, jsonify, request, abort
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retries a specific State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
